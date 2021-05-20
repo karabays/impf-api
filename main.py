4feeeb8b512e249149cbe8f54b12ba4b771c7ae6
Serkan Karabay <serkankarabay@gmail.com>
2021-05-21 00:29:21 +0200

    germany total

--- main.py
+++ main.py
@@ -9,10 +9,10 @@ class Vaccination(BaseModel):
     peopleFullTotal: int
     stateName: str
     population: int
-    vaccinationsPercent: int
+    vaccinationsPercent: float
     peopleFirstPercent: float
     peopleFullPercent: float
-    vaccinesDelivered: float
+    vaccinesDelivered: int
 
 
 state_list = ['bb','be','nd','bw','by','hb','he','hh',
@@ -32,6 +32,10 @@ def load_states():
         code = i.pop('code', None).lower()
         germany_states[code] = i
         germany_states[code]['population'] = int(germany_states[code]['population'])
+    germany_states_list = list(germany_states.values())
+    total_population = sum([i['population'] for i in germany_states_list])
+    germany_states['total'] = {'population':total_population,
+        'english-name': 'Germany Total'}
     return germany_states
 
 
@@ -43,11 +47,23 @@ def load_state_data():
                 if line[key].isdigit():
                     line[key]=int(line[key])
             tsv_data.append(line)
-
     vaccinations_by_state = {}
     for i in tsv_data:
         code = i.pop('code', None).lower()
         vaccinations_by_state[code] = i
+
+    total_tsv_data = []
+    with open("germany_vaccinations_timeseries_v2.tsv", "r") as f:
+        reader = csv.reader(f, delimiter="\t")
+        next(reader, None)
+        for line in reader:
+            total_tsv_data.append([line[1], line[8], line[9]])
+        total_tsv_data = total_tsv_data[-1]
+    
+    vaccinations_by_state['total'] = {'vaccinationsTotal': int(total_tsv_data[0]), 
+        'peopleFirstTotal': int(total_tsv_data[1]), 
+        'peopleFullTotal': int(total_tsv_data[2])}
+    
     return vaccinations_by_state
 
 
@@ -61,10 +77,13 @@ def load_delivery_data():
     c = Counter()
     for l in tsv_data:
         c.update(l)
-    return dict(c)
+    delivery_data = dict(c)
+    delivery_data['total'] = sum(delivery_data.values())
+    
+    return delivery_data
 
 
-def consolidate_data():
+def consolidate_data(code):
     states_data = load_states()
     delivery_data = load_delivery_data()
     vaccinations_by_state = load_state_data()
@@ -75,13 +94,20 @@ def consolidate_data():
         vaccinations_by_state[kode]["peopleFirstPercent"] = round(vaccinations_by_state[kode]["peopleFirstTotal"]*100 / states_data[kode]['population'],1)
         vaccinations_by_state[kode]["peopleFullPercent"] = round(vaccinations_by_state[kode]["peopleFullTotal"]*100 / states_data[kode]['population'],1)
         vaccinations_by_state[kode]["vaccinesDelivered"] = delivery_data[kode]
-    return vaccinations_by_state
+    return vaccinations_by_state[code]
+
+
+@app.get("/total/", response_model=Vaccination)
+def total_data():
+    result =  consolidate_data("total")
+    return result
 
 
 @app.get("/states/", response_model=Vaccination)
 def state_data(state_code: str = Query(..., min_length=2, max_length=2)):
     if state_code in state_list:
-        result =  consolidate_data()["de-"+state_code]
+        result =  consolidate_data("de-"+state_code)
         return result
     else:
         raise HTTPException(404, detail='Invalid state code.')
+
