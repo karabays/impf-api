ee45df22f3cdac7bfb6450e1a70e6242a935b4c2
Serkan Karabay <serkankarabay@gmail.com>
2021-05-21 11:20:12 +0200

    start script

---
+++ start.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+PID=$(ps aux | grep 'uvicorn app.main:app' | grep -v grep | awk {'print $2'} | xargs)
+if [ "$PID" != "" ]
+then
+kill -9 $PID
+sleep 2
+echo "" > nohup.out
+echo "Restarting FastAPI server"
+echo "You can hit ctrl + c now."
+else
+echo "No such process. Starting new FastAPI server"
+echo "You can hit ctrl + c now."
+fi
+nohup /home/ubuntu/.local/bin/uvicorn app.main:app --host '0.0.0.0' --port 8000 &
